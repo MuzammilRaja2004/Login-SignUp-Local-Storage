81924bf4143068ec58fa86652042390dfc435a73
muzammil852 <m.m.muzammilraja@gmail.com>
2021-09-03 22:23:14 -0700

    This Project I Make A Login And SignUp Page With Local Storage

---
+++ JavaScript/app.js
@@ -0,0 +1,140 @@
+const SignUp = () => {
+    // Get Element Id
+    const Username = document.getElementById('username')
+    const Email = document.getElementById('email')
+    const Password = document.getElementById('password')
+
+
+
+
+    // Take A Data In Object
+    const SignUpObject = {
+        UserName: Username.value,
+        Password: Password.value,
+        Email: Email.value,
+    }
+
+
+    const signupdata = JSON.parse(localStorage.getItem("userdata")) || [];
+    signupdata.push(SignUpObject);
+
+
+    // Assign Id In Local Storage
+    localStorage.setItem('userdata', JSON.stringify(signupdata));
+    window.location.assign = "login.html"
+    console.log(SignUpObject)
+
+
+    // Input  Styleing
+    let sign1 = document.getElementById('sign1')
+    sign1.setAttribute("style", "top: 50%;")
+
+    let sign2 = document.getElementById('sign2')
+    sign2.setAttribute("style", "top: 50%;")
+
+    let sign3 = document.getElementById('sign3')
+    sign3.setAttribute("style", "top: 50%;")
+
+    // Validation Condition For Sign Up Page
+    if (Username.value == "") {
+        alert("Username Is Required")
+    }
+    else if (Email.value == "") {
+        alert("Email Is Required")
+    }
+    else if (Password.value == "") {
+        alert("Password Is Required")
+    }
+    else if (Password.length <= 8) {
+        alert("Password Is None Of These  ")
+    }
+    else {
+        alert("Your Are Sign Up SuccessFully")
+    }
+
+    // Remove Value From Input SignUp
+    Username.value = ""
+    Email.value = ""
+    Password.value = ""
+
+}
+
+// Start Login Function
+
+const login = () => {
+    const Email = document.getElementById('email')
+    const Password = document.getElementById('password')
+
+
+    const loginObj = {
+        Email: Email.value,
+        Password: Password.value,
+    }
+
+    var loginUserData = JSON.parse(localStorage.getItem("userdata"))
+
+    let check = false
+    for (var i = 0; i < loginUserData.length; i++) {
+        if (loginUserData[i].Email === loginObj.Email && loginUserData[i].Password === loginObj.Password) {
+            check = true
+        }
+    }
+
+    // Validation Conditions For Sign In Page
+    if (Email.value == "") {
+        alert("Email Is Required")
+    }
+    else if (Password.value == "") {
+        alert("Password Is Required")
+    }
++
+    else {
+    alert("You Are Sign In SuccessFully")
+}
+
+// Remove Styleing
+let label1 = document.getElementById('label1')
+label1.setAttribute("style", "top: 50%;")
+
+let label2 = document.getElementById('label2')
+label2.setAttribute("style", "top: 50%;")
+
+// Remove Value From Input SignUp
+Email.value = ""
+Password.value = ""
+
+
+
+}
+
+// Login Function
+// Function Css Styleing
+const labelEmail = () => {
+    let label1 = document.getElementById('label1')
+    label1.setAttribute("style", "top: -10%;")
+}
+// Label2 Function Css Styleing
+const labelPass = () => {
+    let label2 = document.getElementById('label2')
+    label2.setAttribute("style", "top: -10%;")
+}
+
+// SignUp Function
+// SignUp Label1 Function Css Styleing
+const UserStyle = () => {
+    let sign1 = document.getElementById('sign1')
+    sign1.setAttribute("style", "top: -10%;")
+}
+//  SignUp Label2 Function Css Styleing
+const EmailStyle = () => {
+    let sign2 = document.getElementById('sign2')
+    sign2.setAttribute("style", "top: -10%;")
+}
+
+//  SignUp Label3 Function Css Styleing
+const PassStyle = () => {
+    let sign3 = document.getElementById('sign3')
+    sign3.setAttribute("style", "top: -10%;")
+}
+
+
